79e9df06905d389e042de34ff57a2de6b92828e0
Beasttt1 <amir.mv.7690@gmail.com>
2025-05-20 07:47:11 +0330

    Update index.js

--- index.js
+++ index.js
@@ -481,3 +481,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+  bot.launch().then(() => {
+  console.log('Bot started successfully');
+}).catch(console.error);
