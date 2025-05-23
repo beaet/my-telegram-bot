89d9eacc9505818a1150abb1a27802619c40a09a
Beasttt1 <amir.mv.7690@gmail.com>
2025-05-23 21:01:05 +0330

    Update index.js

--- index.js
+++ index.js
@@ -333,15 +333,6 @@ bot.on('callback_query', async (query) => {
 });
 
   // گرفتن اطلاعات کاربر
-  const user = await getUser(userId);
-  if (!user) {
-    return await bot.answerCallbackQuery(query.id, { text: 'خطا در دریافت اطلاعات کاربر.', show_alert: true });
-  }
-
-  if (user?.banned) {
-    return await bot.answerCallbackQuery(query.id, { text: 'شما بن شده‌اید و اجازه استفاده ندارید.', show_alert: true });
-  }
-
   if (data === 'main_menu') {
     await bot.answerCallbackQuery(query.id);
     sendMainMenu(userId, messageId);
